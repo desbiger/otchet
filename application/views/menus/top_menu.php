7fb685098a5689f1f1fc0d3a3a4eb65395ef943a
desbiger <desbiger@gmail.com>
2014-08-25 19:09:16 +0400

    Добавлено : формирование списка задач в выпадающем меню по проектам

--- application/views/menus/top_menu.php
+++ application/views/menus/top_menu.php
@@ -41,7 +41,7 @@
 				2,
 				0
 		))
-		->order_by('tasks.dead_line')
+		->order_by('tasks.project_id')
 		->find_all();?>
 <style type = "text/css">
 	.procent {
@@ -50,23 +50,56 @@
 </style>
 <ul class = "dop_menu">
 	<li>
-		<?if($tasks_w->count()):?>
-		<div class = "count"><?= $tasks_w->count() ?></div>
-		<?endif?>
+		<? if ($tasks_w->count()): ?>
+			<div class = "count"><?= $tasks_w->count() ?></div>
+		<? endif ?>
 		<a href = "/">
 			<img src = "/include/img/taskbar.png" alt = ""/><br/>
 			Мои задачи
-			<ul>
-				<? foreach ($tasks_w as $t): ?>
-					<li class = "<?= My::$status_color[$t->status] ?>">
-						<a href = "/projects/taskdetail/<?= $t->project_id ?>/<?= $t->id ?>"><?=
-								$t->name ?>(<?= $t->project->name ?>)<br/><?
-								My::statusLine($t->finish, My::$status_color[$t->status])
-							?> <?= $t->finish ?>%</a>
-					</li>
-				<? endforeach ?>
-			</ul>
 		</a>
+		<ul>
+			<? $cur_project = 0; ?>
+
+			<? foreach ($tasks_w as $k => $t): ?>
+				<? if ($k == 0 || $cur_project != $t->project->id): ?>
+					<li class = "blue title"><?= $t->project->name ?></li>
+					<li class = "<?= My::$status_color[$t->status] ?>">
+						<table>
+							<tr>
+								<td>
+									<a href = "/projects/taskdetail/<?= $t->project_id ?>/<?= $t->id ?>"><?=
+											$t->name ?></a>
+								</td>
+								<td>
+									<?
+										My::statusLine($t->finish, My::$status_color[$t->status])
+									?> <?= $t->finish ?>%
+								</td>
+							</tr>
+						</table>
+					</li>
+					<? $cur_project = $t->project->id ?>
+				<? else: ?>
+					<li class = "<?= My::$status_color[$t->status] ?>">
+						<table>
+							<tr>
+								<td>
+									<a href = "/projects/taskdetail/<?= $t->project_id ?>/<?= $t->id ?>"><?=
+											$t->name ?></a>
+								</td>
+								<td>
+									<?
+										My::statusLine($t->finish, My::$status_color[$t->status])
+									?> <?= $t->finish ?>%
+								</td>
+							</tr>
+						</table>
+					</li>
+				<?endif ?>
+
+			<? endforeach ?>
+		</ul>
+
 	</li>
 	<li>
 
@@ -82,13 +115,44 @@
 			<img src = "/include/img/taskbar.png" alt = ""/><br/>
 			Коментарии
 			<ul>
+				<? $cur_project = 0; ?>
 				<? foreach (Comments::Factory()
 						            ->NewComments() as $comments): ?>
-					<li>
-						<a href = "/projects/taskdetail/<?= $comments->task->project_id ?>/<?= $comments->task->id ?>"><?=
-								$comments->task->name
-							?></a>
-					</li>
+					<? if ($cur_project != $comments->project->id): ?>
+						<li class = "blue title"><?= $comments->project->name ?></li>
+						<li class = "<?= My::$status_color[$comments->status] ?>">
+							<table>
+								<tr>
+									<td>
+										<a href = "/projects/taskdetail/<?= $comments->project_id ?>/<?= $comments->id ?>"><?=
+												$comments->name ?></a>
+									</td>
+									<td>
+										<?
+											My::statusLine($comments->finish, My::$status_color[$comments->status])
+										?> <?= $comments->finish ?>%
+									</td>
+								</tr>
+							</table>
+						</li>
+						<? $cur_project = $comments->project->id ?>
+					<? else: ?>
+						<li class = "<?= My::$status_color[$comments->status] ?>">
+							<table>
+								<tr>
+									<td>
+										<a href = "/projects/taskdetail/<?= $comments->project_id ?>/<?= $comments->id ?>"><?=
+												$comments->name ?></a>
+									</td>
+									<td>
+										<?
+											My::statusLine($comments->finish, My::$status_color[$comments->status])
+										?> <?= $comments->finish ?>%
+									</td>
+								</tr>
+							</table>
+						</li>
+					<?endif ?>
 				<? endforeach ?>
 			</ul>
 		</a>
@@ -112,23 +176,51 @@
 			<a href = "">
 				<img src = "/include/img/taskbar.png" alt = ""/><br/>
 				Я поручил(а)
-				<ul>
-					<? foreach (Task::FindMyTasks(array(
-							0,
-							2
-					)) as $task): ?>
-						<li>
-							<a href = "/projects/taskdetail/<?= $task->project_id ?>/<?= $task->id ?>"><?=
-									$task->name
-								?>
-								<br/>
-								<?My::statusLine($task->finish, My::$status_color[$task->status])
-								?> <?= $task->finish ?>%
-							</a>
-						</li>
-					<? endforeach ?>
-				</ul>
 			</a>
+			<ul>
+				<? $cur_project = 0; ?>
+				<? foreach (Task::FindMyTasks(array(
+						0,
+						2
+				)) as $task): ?>
+					<? if ($cur_project != $task->project->id): ?>
+						<li class = "blue title"><?= $task->project->name ?></li>
+						<li class = "<?= My::$status_color[$task->status] ?>">
+							<table>
+								<tr>
+									<td>
+										<a href = "/projects/taskdetail/<?= $task->project_id ?>/<?= $task->id ?>"><?=
+												$task->name ?></a>
+									</td>
+									<td>
+										<?
+											My::statusLine($task->finish, My::$status_color[$task->status])
+										?> <?= $task->finish ?>%
+									</td>
+								</tr>
+							</table>
+						</li>
+						<? $cur_project = $task->project->id ?>
+					<? else: ?>
+						<li class = "<?= My::$status_color[$task->status] ?>">
+							<table>
+								<tr>
+									<td>
+										<a href = "/projects/taskdetail/<?= $task->project_id ?>/<?= $task->id ?>"><?=
+												$task->name ?></a>
+									</td>
+									<td>
+										<?
+											My::statusLine($task->finish, My::$status_color[$task->status])
+										?> <?= $task->finish ?>%
+									</td>
+								</tr>
+							</table>
+						</li>
+					<?endif ?>
+				<? endforeach ?>
+			</ul>
+
 		</li>
 	<? endif ?>
 </ul>
