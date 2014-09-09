7a4eb915a7687e549de20de65cdb828778ada34c
desbiger <desbiger@gmail.com>
2014-09-09 11:46:16 +0400

    разграничение проектов по типу

--- application/views/forms/user_profile_edit.php
+++ application/views/forms/user_profile_edit.php
@@ -16,11 +16,17 @@
 			<img src = "<?= $user->avatar ? $user->avatar : '/include/empty_ava.png' ?>" alt = ""/>
 		</td>
 		<td>
-			<form action = "" method="post">
+			<form action = "" method = "post">
 				<div class = "user_about">
+					<p>Имя: <span><?= Form::input('name', $user->name) ?></span></p>
+
+					<p>Фамилия: <span><?= Form::input('firstname', $user->firstname) ?></span></p>
+
+					<p>Отчество: <span><?= Form::input('secondename', $user->secondename) ?></span></p>
+
 					<p>Должность: <span><?= Form::input('dolzhnost', $user->dolzhnost) ?></span></p>
 
-					<p>Дата рождения: <span><?= Form::input('berthday', $user->berthday,array('class'=>'date')) ?></span></p>
+					<p>Дата рождения: <span><?= Form::input('berthday', $user->berthday, array('class' => 'date')) ?></span></p>
 
 					<p>Электронная почта: <span><?= Form::input('email', $user->user->email) ?></span></p>
 
@@ -30,18 +36,23 @@
 
 					<p>Пол: <span><?= Form::input('sex', $user->sex) ?></span></p>
 				</div>
-				<p><?=Form::submit('update','Обновить данные')?></p>
+				<p><?= Form::submit('update', 'Обновить данные') ?></p>
 			</form>
 		</td>
 		<td>
-			<form action = "" method="post">
-				<div class="user_about">
-					<p><h2>Смена пароля</h2></p>
-					<i><?=Arr::get($errors,'password')?></i>
-					<p>Новый пароль <span><?=Form::password('new_password')?></span></p>
-					<i><?=Arr::get($errors,'password_confirm')?></i>
-					<p>Новый пароль еще раз<span><?=Form::password('new_password_confirm')?></span></p>
-					<p><?=Form::submit('change_pass','Сменить пароль')?></p>
+			<form action = "" method = "post">
+				<div class = "user_about">
+					<p>
+
+					<h2>Смена пароля</h2></p>
+					<i><?= Arr::get($errors, 'password') ?></i>
+
+					<p>Новый пароль <span><?= Form::password('new_password') ?></span></p>
+					<i><?= Arr::get($errors, 'password_confirm') ?></i>
+
+					<p>Новый пароль еще раз<span><?= Form::password('new_password_confirm') ?></span></p>
+
+					<p><?= Form::submit('change_pass', 'Сменить пароль') ?></p>
 				</div>
 			</form>
 		</td>
--- application/views/tables/objects.php
+++ application/views/tables/objects.php
@@ -1,30 +1,178 @@
-<? $objects = ORM::factory('Objects')
-		->order_by('name')
-		->find_all()?>
+<style type = "text/css">
+	.base_table{
+		margin-top: 10px;
+	}
+</style>
 <h2>Список проектов
 	<i class = "project" style = "float: right"></i>
 </h2>
 <a href = "#add_project" class = "fancy plus" title = "Добавить проект"></a>
-<table class = "base_table">
-	<tr>
-		<td>Название объекта</td>
-		<td>Статус проекта</td>
-		<td>Описание объекта</td>
-		<td>Ваших задач в работе</td>
-	</tr>
-	<? foreach ($objects as $vol): ?>
-		<?$tasks = ORM::factory('Tasks')
-				->join('task_workers')
-				->on('task_workers.tasks_id', '=', 'tasks.id')
-				->where('task_workers.worker_id', '=', WORKER_ID)
-				->where('status', '=', 2)
-				->where('project_id', '=', $vol->id)
-				->find_all();?>
-		<tr <?= $tasks->count() ? "class='blue'" : '' ?>>
-			<td style = "font-weight: bold; font-size: 14px"><a href = "/projects/project/<?= $vol->id ?>"><?= $vol->name ?></a></td>
-			<td><?=$vol->stat->name?></td>
-			<td><?= $vol->description ?></td>
-			<td style = "text-align: center"><?= $tasks->count() ?></td>
-		</tr>
-	<? endforeach ?>
-</table>
+<div class = "tabs">
+	<ul class = "tabNavigation">
+		<li>
+			<a href = "#work">В разработке</a>
+		</li>
+		<li>
+			<a href = "#pod">На поддержке</a>
+		</li>
+		<li>
+			<a href = "#seo">На продвижении</a>
+		</li>
+		<li>
+			<a href = "#w">В ожидании</a>
+		</li>
+		<li>
+			<a href = "#archive">Архив</a>
+		</li>
+	</ul>
+	<div id = "work">
+		<table class = "base_table">
+			<tr>
+				<td>Название объекта</td>
+				<td>Статус проекта</td>
+				<td>Описание объекта</td>
+				<td>Ваших задач в работе</td>
+			</tr>
+			<? $objects = ORM::factory('Objects')
+					->where('status', '=', 1)
+					->order_by('name')
+					->find_all()?>
+			<? foreach ($objects as $vol): ?>
+				<?$tasks = ORM::factory('Tasks')
+						->join('task_workers')
+						->on('task_workers.tasks_id', '=', 'tasks.id')
+						->where('task_workers.worker_id', '=', WORKER_ID)
+						->where('status', '=', 2)
+						->where('project_id', '=', $vol->id)
+						->find_all();?>
+				<tr <?= $tasks->count() ? "class='blue'" : '' ?>>
+					<td style = "font-weight: bold; font-size: 14px"><a href = "/projects/project/<?= $vol->id ?>"><?= $vol->name ?></a>
+					</td>
+					<td><?= $vol->stat->name ?></td>
+					<td><?= $vol->description ?></td>
+					<td style = "text-align: center"><?= $tasks->count() ?></td>
+				</tr>
+			<? endforeach ?>
+		</table>
+	</div>
+	<div id = "pod">
+		<table class = "base_table">
+					<tr>
+						<td>Название объекта</td>
+						<td>Статус проекта</td>
+						<td>Описание объекта</td>
+						<td>Ваших задач в работе</td>
+					</tr>
+					<? $objects = ORM::factory('Objects')
+							->where('status', '=', 4)
+							->order_by('name')
+							->find_all()?>
+					<? foreach ($objects as $vol): ?>
+						<?$tasks = ORM::factory('Tasks')
+								->join('task_workers')
+								->on('task_workers.tasks_id', '=', 'tasks.id')
+								->where('task_workers.worker_id', '=', WORKER_ID)
+								->where('status', '=', 2)
+								->where('project_id', '=', $vol->id)
+								->find_all();?>
+						<tr <?= $tasks->count() ? "class='blue'" : '' ?>>
+							<td style = "font-weight: bold; font-size: 14px"><a href = "/projects/project/<?= $vol->id ?>"><?= $vol->name ?></a>
+							</td>
+							<td><?= $vol->stat->name ?></td>
+							<td><?= $vol->description ?></td>
+							<td style = "text-align: center"><?= $tasks->count() ?></td>
+						</tr>
+					<? endforeach ?>
+				</table>
+	</div>
+	<div id = "seo">
+		<table class = "base_table">
+					<tr>
+						<td>Название объекта</td>
+						<td>Статус проекта</td>
+						<td>Описание объекта</td>
+						<td>Ваших задач в работе</td>
+					</tr>
+					<? $objects = ORM::factory('Objects')
+							->where('status', '=', 5)
+							->order_by('name')
+							->find_all()?>
+					<? foreach ($objects as $vol): ?>
+						<?$tasks = ORM::factory('Tasks')
+								->join('task_workers')
+								->on('task_workers.tasks_id', '=', 'tasks.id')
+								->where('task_workers.worker_id', '=', WORKER_ID)
+								->where('status', '=', 2)
+								->where('project_id', '=', $vol->id)
+								->find_all();?>
+						<tr <?= $tasks->count() ? "class='blue'" : '' ?>>
+							<td style = "font-weight: bold; font-size: 14px"><a href = "/projects/project/<?= $vol->id ?>"><?= $vol->name ?></a>
+							</td>
+							<td><?= $vol->stat->name ?></td>
+							<td><?= $vol->description ?></td>
+							<td style = "text-align: center"><?= $tasks->count() ?></td>
+						</tr>
+					<? endforeach ?>
+				</table>
+	</div>
+	<div id = "w">
+		<table class = "base_table">
+					<tr>
+						<td>Название объекта</td>
+						<td>Статус проекта</td>
+						<td>Описание объекта</td>
+						<td>Ваших задач в работе</td>
+					</tr>
+					<? $objects = ORM::factory('Objects')
+							->where('status', '=', 2)
+							->order_by('name')
+							->find_all()?>
+					<? foreach ($objects as $vol): ?>
+						<?$tasks = ORM::factory('Tasks')
+								->join('task_workers')
+								->on('task_workers.tasks_id', '=', 'tasks.id')
+								->where('task_workers.worker_id', '=', WORKER_ID)
+								->where('status', '=', 2)
+								->where('project_id', '=', $vol->id)
+								->find_all();?>
+						<tr <?= $tasks->count() ? "class='blue'" : '' ?>>
+							<td style = "font-weight: bold; font-size: 14px"><a href = "/projects/project/<?= $vol->id ?>"><?= $vol->name ?></a>
+							</td>
+							<td><?= $vol->stat->name ?></td>
+							<td><?= $vol->description ?></td>
+							<td style = "text-align: center"><?= $tasks->count() ?></td>
+						</tr>
+					<? endforeach ?>
+				</table>
+	</div>
+	<div id = "archive">
+		<table class = "base_table">
+					<tr>
+						<td>Название объекта</td>
+						<td>Статус проекта</td>
+						<td>Описание объекта</td>
+						<td>Ваших задач в работе</td>
+					</tr>
+					<? $objects = ORM::factory('Objects')
+							->where('status', '=', 3)
+							->order_by('name')
+							->find_all()?>
+					<? foreach ($objects as $vol): ?>
+						<?$tasks = ORM::factory('Tasks')
+								->join('task_workers')
+								->on('task_workers.tasks_id', '=', 'tasks.id')
+								->where('task_workers.worker_id', '=', WORKER_ID)
+								->where('status', '=', 2)
+								->where('project_id', '=', $vol->id)
+								->find_all();?>
+						<tr <?= $tasks->count() ? "class='blue'" : '' ?>>
+							<td style = "font-weight: bold; font-size: 14px"><a href = "/projects/project/<?= $vol->id ?>"><?= $vol->name ?></a>
+							</td>
+							<td><?= $vol->stat->name ?></td>
+							<td><?= $vol->description ?></td>
+							<td style = "text-align: center"><?= $tasks->count() ?></td>
+						</tr>
+					<? endforeach ?>
+				</table>
+	</div>
+</div>
