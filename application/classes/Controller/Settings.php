cafcf9a536718036c2440c65cdbb2f0160137e1c
pavel <desbiger@gmail.com>
2014-12-05 12:21:05 +0300

    Контроллер настроек модулей

--- application/classes/Controller/Settings.php
+++ application/classes/Controller/Settings.php
@@ -1,7 +1,11 @@
 <?php defined('SYSPATH') or die('No direct script access.');
-    class Controller_Settings extends Controller_Base
-    {
-        public function action_index(){
-            
-        }
-    }
+
+	class Controller_Settings extends Controller_Base
+	{
+		public function action_index()
+		{
+			$imap                    = Settings::factory('imap')
+					->GetVar('pass');
+			$this->template->content = $imap;
+		}
+	}
