12f2b454fddb207764d1465283b1940abe899036
pavel <desbiger@gmail.com>
2014-12-08 18:41:28 +0300

    Внедрение ограничений доступа к списку склиентов

--- application/views/blocks/clients_menu.php
+++ application/views/blocks/clients_menu.php
@@ -3,9 +3,10 @@
 		->find_all() ?>
 <ul class = "projects">
 	<? foreach ($clients as $p): ?>
-
+		<?if(Acl::CheckClientAccess($p->id)):?>
 		<li <?= $p->id == $id ? "class='curent_project'" : '' ?>>
 			<a href = "/clients/client/<?= $p->id ?>" <?= $p->id == $id ? "style='font-weight:bold'" : '' ?>><?= $p->name ?></a>
 		</li>
+			<?endif?>
 	<? endforeach ?>
 </ul>
