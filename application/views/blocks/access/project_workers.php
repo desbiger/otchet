b0af9b1873657fce630516e78d4d47c0a802520e
pavel <desbiger@gmail.com>
2014-12-11 15:43:47 +0300

    Ajax при добавлении пользователя к проекту

--- application/views/blocks/access/project_workers.php
+++ application/views/blocks/access/project_workers.php
@@ -1,7 +1,23 @@
+<script type="text/javascript">
+	$(function () {
+		$('a.user_add').click(function () {
+
+			var worker_id = $(this).attr('id');
+			var project_id = $(this).attr('project');
+			$('.conteyner').html('<img src="/include/img/ajax_loading.gif" />');
+			$.get('/ajax/shareProject/?worker_id=' + worker_id + '&project_id=' + project_id, function (data) {
+				$.fancybox.close();
+				$('.conteyner').html(data);
+			});
+		});
+	})
+</script>
 <h2>Настройки доступа к проекту</h2>
-<a class="plus" href="#"></a>
-<ul class="project_worker">
+<a class="plus fancy" href="#users"></a>
+<ul class="project_worker conteyner">
+	<?$in_project = array(0);?>
 	<? foreach ($project_users as $user): ?>
+		<? $in_project[] = $user->worker->id ?>
 		<li>
 			<a href="">
 				<div class="ava">
@@ -12,3 +28,22 @@
 		</li>
 	<? endforeach ?>
 </ul>
+<div style="display: none" class="not_in_access">
+	<? $users = ORM::factory('Workers')
+			->where('id', 'NOT IN', $in_project)
+			->find_all() ?>
+	<div id="users">
+		<ul class="project_worker">
+			<? foreach ($users as $vol): ?>
+				<li>
+					<a href="#" class="user_add" id="<?= $vol->id ?>" project="<?= $project_id ?>">
+						<div class="ava">
+							<img src="<?= Worker::GetAva($vol->id, 130, 200) ?>" alt=""/>
+						</div>
+						<?= $vol->name ?> <?= $vol->firstname ?>
+					</a>
+				</li>
+			<? endforeach ?>
+		</ul>
+	</div>
+</div>
